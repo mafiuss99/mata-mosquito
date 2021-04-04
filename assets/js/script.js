331c971ec9ba6ed52a7aadbfd57fdf6c815f1735
Matheus Felipe <mf.mf98.23@gmail.com>
2021-04-04 01:11:04 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,93 @@
+var pontuacoes = [];
+
+function loadSplashScreen(){
+    var logo =  document.querySelector(".mosquito-animado");
+    var splash = document.querySelector(".splash-screen");
+    splash.style.display = "flex";
+    var loadAnimate = setInterval(function(){
+        logo.style.top = logo.style.top == "0px" ? "30px" : 0;
+    }, 400);
+
+    setTimeout(
+        function(){
+            splash.style.display = "none";
+            clearInterval(loadAnimate)
+        }, 3000
+    )
+}
+
+function loadHome(){
+    loadSplashScreen();
+
+    document.getElementById("sessaoHome").classList.add("active");
+
+    var logo =  document.querySelector(".mosquito-animado-home");
+    setInterval(function(){
+        logo.style.top = logo.style.top == "0px" ? "30px" : 0;
+    }, 400);
+}
+
+function loadModalCadastro(){
+    let modal = document.getElementById("modalComecar");
+    modal.style.display = "block";
+}
+
+function cadastrar(){
+    let usuario = document.getElementById("nomeUsuario").value;
+    pontuacoes.push("{'usuario': "+  usuario +"}");
+    
+    let idUser = document.getElementById("identificadorUser");
+    idUser.value = pontuacoes.length;
+
+    closeModalCadastro();
+    loadGame()
+}
+
+function loadGame(){
+    loadSplashScreen();
+
+    var activieselements = document.getElementsByClassName("active")[0];
+    activieselements.classList.remove("active");
+
+    document.getElementById("sessaoGame").classList.add("active");
+
+    voar(400)
+
+}
+
+function closeModalCadastro(){
+    let modal = document.getElementById("modalComecar");
+    modal.style.display = "none";
+}
+
+function voar(velocidade){
+    var audio = new Audio('audio/zumbido.mp3');
+    audio.addEventListener('canplaythrough', function() {
+        audio.play();
+    });
+    var mosquito = document.getElementById("mosquito");
+    
+    var voador = setInterval(function(){
+        let px = Math.random()*500 + "px";
+        let py = Math.random()*500 + "px";
+
+        mosquito.style.top = py;
+        mosquito.style.left = px;
+        console.log("alou")
+    }, velocidade);
+
+    mosquito.setAttribute("onclick", "matar("+voador+")")
+
+}
+
+function matar(paremeter){
+    clearInterval(paremeter);
+    var mosquito = document.getElementById("mosquito");
+
+    mosquito.style.backgroundImage = "url(img/mancha-vermelha.gif)"
+}
+
+window.addEventListener("load", function(){
+    
+    loadHome();
+})
